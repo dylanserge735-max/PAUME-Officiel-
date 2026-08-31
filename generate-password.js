926ba079c4048852ba8d512addf836668b2e1b7d
dylanserge735-max <dylanserge735@gmail.com>
2026-08-31 13:36:05 +0000

    Update generate-password.js

--- generate-password.js
+++ generate-password.js
@@ -1,1 +1,18 @@
+import crypto from "crypto";
 
+const password = process.argv[2];
+
+if (!password) {
+  console.error("Utilisation : node generate-password.js \"VotreMotDePasse\"");
+  process.exit(1);
+}
+
+const salt = crypto.randomBytes(16).toString("hex");
+
+const hash = crypto
+  .scryptSync(password, salt, 64)
+  .toString("hex");
+
+console.log("\nHASH À ENREGISTRER DANS NEON :\n");
+console.log(`${salt}:${hash}`);
+console.log("");
